dc2d6ba9a5bfb9237de9bb2ef5fa0abfa7d3a32a
henrique <gabgba90@gmail.com>
2022-10-14 02:46:38 -0300

    teste3

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,8 @@ import './styles/global.css';
 
 export function App() {
  return (
-  <>
-    <h1 className="font-bold text-2xl text-violet-500">hello world</h1>
-
-    <button className="bg-beije-900 font-medium px-4 py-3 rounded text-white hover:bg-violet-600">
-      enviar
-    </button>
-    </>
+      <div className="w-screen h-screen bg-gray-900 flex itens-center justify-center text-white">
+      </div>
   )
 }
 
